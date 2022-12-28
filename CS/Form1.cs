1af008ead7c52f8cdbc755e126ea14a34873e9d8
Dmitry Romenskiy (DevExpress) <romenskiy.dmitry@devexpress.com>
2022-12-28 18:04:49 +0400

    init repo

---
+++ CS/Form1.cs
@@ -0,0 +1,26 @@
+﻿using DevExpress.Utils;
+using DevExpress.XtraBars;
+using System;
+using System.Linq;
+
+namespace dxSample {
+    public partial class Form1 : DevExpress.XtraEditors.XtraForm {
+        Random r = new Random();
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void barEditItem1_EditValueChanged(object sender, EventArgs e) {
+            var editItem = sender as BarEditItem;
+            var value = Convert.ToInt32(editItem.EditValue);
+            PointFloat[] points = new PointFloat[value];
+
+            for (int i = 0; i < value; i++) {
+                points[i] = new PointFloat() { X = Convert.ToSingle(r.NextDouble()), Y = r.Next(0, 2) == 0 ? 0 : 1 };
+            }
+
+            diagramShape1.ConnectionPoints = new DevExpress.XtraDiagram.PointCollection(points);
+        }
+    }
+}
